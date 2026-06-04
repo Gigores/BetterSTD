91965ca0de0dd93f6598cdf9024706e33a9502ec
Priklen <igorsamodumskij@gmail.com>
2026-06-04 22:11:14 +0300

    reformat

--- include/btrstd/bllist.h
+++ include/btrstd/bllist.h
@@ -3,11 +3,9 @@
 #include "stddef.h"
 #include "stdbool.h"
 
-/**
- * The idea of the Borrowing Linked List is that it does not own the data,
- * it only points to data that you own.
- * So it is your responsibility to allocate and free the data.
- */
+// The idea of the Borrowing Linked List is that it does not own the data,
+// it only points to data that you own.
+// So it is your responsibility to allocate and free the data.
 
 typedef struct BLListNode {
     void *payload;
@@ -19,94 +17,61 @@ typedef struct {
     size_t size;
 } bllist_t;
 
-/**
- * Creates a new borrowing linked list from an array of given values.
- * Example:
- * ```c
- * int a = 20;
- * int b = 26;
- * int c = 43;
- * bllist_t list = BLList_make({&a, &b, &c}, 3);
- * ```
- */
+// Creates a new borrowing linked list from an array of given values.
+// Example:
+// ```c
+// int a = 20;
+// int b = 26;
+// int c = 43;
+// bllist_t list = BLList_make({&a, &b, &c}, 3);
+// ```
 bllist_t BLList_make(void *items[], size_t itemCount);
-/**
- * Creates a new borrowing linked list from another borrowing linked list with the same data.
- */
+// Creates a new borrowing linked list from another borrowing linked list with the same data.
 bllist_t BLList_clone(const bllist_t *list);
-/**
- * Appends the data to the end of the borrowing linked list.
- */
+// Appends the data to the end of the borrowing linked list.
 void BLList_append(bllist_t *, void *data);
-/**
- * Prepends the data to the beginning of the borrowing linked list.
- */
+// Prepends the data to the beginning of the borrowing linked list.
 void BLList_prepend(bllist_t *, void *data);
-/**
- * Inserts an item so that it's index is the specified one.
- */
+// Inserts an item so that it's index is the specified one.
 void BLList_insert(bllist_t *, void *data, long index);
-/**
- * Pops the data of the specified index from a borrowing linked list and returns it.
- * Can accept negative indexes.
- */
+// Pops the data of the specified index from a borrowing linked list and returns it.
+// Can accept negative indexes.
 void *BLList_pop(bllist_t *, long index);
-/**
- * Returns the data of the specified index of the borrowing linked list.
- * Returns `NULL` if the index is invalid.
- * Returns `NULL` if the pointer to the borrowing linked list is invalid.
- * Can accept negative indexes.
- */
+// Returns the data of the specified index of the borrowing linked list.
+// Returns `NULL` if the index is invalid.
+// Returns `NULL` if the pointer to the borrowing linked list is invalid.
+// Can accept negative indexes.
 void *BLList_get(const bllist_t *, long index);
-/**
- * Returns the first item of the borrowing linked list.
- * Returns `NULL` if the list is empty.
- * Returns `NULL` if the pointer to the borrowing linked list is invalid.
- */
+// Returns the first item of the borrowing linked list.
+// Returns `NULL` if the list is empty.
+// Returns `NULL` if the pointer to the borrowing linked list is invalid.
 void *BLList_first(const bllist_t *);
-/**
- * Returns the last item of the borrowing linked list.
- * Returns `NULL` if the list is empty.
- * Returns `NULL` if the pointer to the borrowing linked list is invalid.
- */
+// Returns the last item of the borrowing linked list.
+// Returns `NULL` if the list is empty.
+// Returns `NULL` if the pointer to the borrowing linked list is invalid.
 void *BLList_last(const bllist_t *);
-/**
- * Returns the index of the first occurance of the item.
- * Returns -1 if the item wasn't found.
- * Returns -1 if the pointer to the borrowing linked list is invalid.
- */
+// Returns the index of the first occurance of the item.
+// Returns -1 if the item wasn't found.
+// Returns -1 if the pointer to the borrowing linked list is invalid.
 long BLList_indexOf(bllist_t *list, void *value, bool (*cmp)(const void *, const void *));
-/**
- * Returns the amount of items in the borrowing linked list.
- * Returns -1 if the pointer to the borrowing linked list is invalid.
- */
+// Returns the amount of items in the borrowing linked list.
+// Returns -1 if the pointer to the borrowing linked list is invalid.
 size_t BLList_len(const bllist_t *);
-/**
- * Tells if the borrowing linked list is empty.
- * Returns -1 if the pointer to the borrowing linked list is invalid.
- */
+// Tells if the borrowing linked list is empty.
+// Returns -1 if the pointer to the borrowing linked list is invalid.
 bool BLList_isEmpty(const bllist_t *);
-/**
- * Reverses the order of items in the borrowing linked list.
- * Returns `false` if the pointer to the borrowing linked list is invalid.
- */
+// Reverses the order of items in the borrowing linked list.
+// Returns `false` if the pointer to the borrowing linked list is invalid.
 void BLList_reverse(bllist_t *);
-/**
- * Deallocates the borrowing linked list.
- */
+// Deallocates the borrowing linked list.
 void BLList_free(bllist_t *);
-/**
- * The exact same as `BLList_free`
- */
+// The exact same as `BLList_free`
 void BLList_clear(bllist_t *);
-/**
- * Example usage:
- * ```c
- * bllist_t list = {...};  // a list of strings
- * BLLIST_FOREACH(list, item)
- *     printf("%s\n", (char *)item);
- * ```
- */
+// Example usage:
+// ```c
+// bllist_t list = {...};  // a list of strings
+// BLLIST_FOREACH(list, item)
+//     printf("%s\n", (char *)item);
 #define BLLIST_FOREACH(LIST, i)           \
     void *i;                                  \
     for (                                     \
@@ -114,14 +79,12 @@ void BLList_clear(bllist_t *);
         _n != NULL && ((i = _n->payload), 1); \
         _n = _n->next                         \
     )
-/**
- * Example usage:
- * ```c
- * bllist_t list = {...};  // a list of strings
- * BLLIST_ENUMERATE(list, item, index)
- *     printf("%zu: %s\n", index, (char *)item);
- * ```
- */
+// Example usage:
+// ```c
+// bllist_t list = {...};  // a list of strings
+// BLLIST_ENUMERATE(list, item, index)
+//     printf("%zu: %s\n", index, (char *)item);
+// ```
 #define BLLIST_ENUMERATE(LIST, i, n)      \
     size_t n = 0;                             \
     void *i;                                  \
@@ -131,13 +94,11 @@ void BLList_clear(bllist_t *);
         _i = _i->next, n++                    \
     )
 
-/**
- * Example usage:
- * ```c
- * int a = 14, b = 43, c = 34;
- * bre_bllist_t list = BLLIST(&a, &b, &c);
- * ```
- */
+// Example usage:
+// ```c
+// int a = 14, b = 43, c = 34;
+// bre_bllist_t list = BLLIST(&a, &b, &c);
+// ```
 #define BLLIST(...)                                    \
     BLList_make(                                        \
         (void *[]){ __VA_ARGS__ },                         \
--- include/btrstd/bstring.h
+++ include/btrstd/bstring.h
@@ -18,105 +18,61 @@ typedef struct {
     unsigned int capacity;
 } string_view_t;
 
-/**
- * Creates a string from an array of bytes.
- * Can return a zero-initialised structure if fails to allocate memory.
- */
+// Creates a string from an array of bytes.
+// Can return a zero-initialised structure if fails to allocate memory.
 string_t String_clone(const char *chars);
-/**
- * Crops the given count of characters from the left of the string.
- * "გვიპყრობდა" -> crop 3 -> "პყრობდა"
- */
+// Crops the given count of characters from the left of the string.
+// "გვიპყრობდა" -> crop 3 -> "პყრობდა"
 void String_cropLeft(string_t *, unsigned int charCount);
-/**
- * Crops the given count of characters from the right of the string.
- * "გვიპყრობდა" -> crop 3 -> "გვიპყრო"
- */
+// Crops the given count of characters from the right of the string.
+// "გვიპყრობდა" -> crop 3 -> "გვიპყრო"
 void String_cropRight(string_t *, unsigned int charCount);
-/**
- * Compares the two given strings.
- * Returns 0 if they are equal.
- */
+// Compares the two given strings.
+// Returns 0 if they are equal.
 int String_compare(string_t *, string_t *);
-/**
- * Compares the given string and a string view.
- * Returns 0 if they are equal.
- */
+// Compares the given string and a string view.
+// Returns 0 if they are equal.
 int String_compareView(string_t *, string_view_t *);
-/**
- * Retuns a string view of the string.
- */
+// Retuns a string view of the string.
 string_view_t String_getView(string_t *);
-/**
- * Frees the memory of a string.
- */
+// Frees the memory of a string.
 void String_free(string_t *);
 
-/**
- * Creates a string view from an array of bytes.
- * The string in question will not own the given data.
- */
+// Creates a string view from an array of bytes.
+// The string in question will not own the given data.
 string_view_t StringView_fromCString(const char *chars);
-/**
- * Crops the given count of characters from the left of the string view.
- * "გვიპყრობდა" -> crop 3 -> "პყრობდა"
- */
+// Crops the given count of characters from the left of the string view.
+// "გვიპყრობდა" -> crop 3 -> "პყრობდა"
 void StringView_cropLeft(string_view_t *, unsigned int charCount);
-/**
- * Crops the given count of characters from the right of the string view.
- * "გვიპყრობდა" -> crop 3 -> "გვიპყრო"
- */
+// Crops the given count of characters from the right of the string view.
+// "გვიპყრობდა" -> crop 3 -> "გვიპყრო"
 void StringView_cropRight(string_view_t *, unsigned int charCount);
-/**
- * Reverts the given count of characters from the left of the string view, that were previously cropped.
- * "გვიპყრობდა" -> crop 3 -> "პყრობდა" -> revert 2 "ვიპყრობდა"
- */
+// Reverts the given count of characters from the left of the string view, that were previously cropped.
+// "გვიპყრობდა" -> crop 3 -> "პყრობდა" -> revert 2 "ვიპყრობდა"
 void StringView_revertLeft(string_view_t *, unsigned int charCount);
-/**
- * Reverts the given count of characters from the right of the string view, that were previously cropped.
- * "გვიპყრობდა" -> crop 3 -> "გვიპყრო" -> revert 2 -> "გვიპყრობდ"
- */
+// Reverts the given count of characters from the right of the string view, that were previously cropped.
+// "გვიპყრობდა" -> crop 3 -> "გვიპყრო" -> revert 2 -> "გვიპყრობდ"
 void StringView_revertRight(string_view_t *, unsigned int charCount);
-/**
- * Returns the size of a view IN BYTES.
- */
+// Returns the size of a view IN BYTES.
 size_t StringView_byteCount(string_view_t *);
-/**
- * Returns the size of a view IN CHARACTERS.
- */
+// Returns the size of a view IN CHARACTERS.
 size_t StringView_len(string_view_t *);
-/**
- * Tells if the string view is empty.
- */
+// Tells if the string view is empty.
 bool StringView_isEmpty(string_view_t *);
-/**
- * Returns a pointer to a beginning of a UTF-8 character.
- * Returns `NULL` if the pointer is invalid.
- */
+// Returns a pointer to a beginning of a UTF-8 character.
+// Returns `NULL` if the pointer is invalid.
 const char *StringView_charAt(string_view_t *, int index);
-/**
- * Tells if the string view ends with another string view.
- */
+// Tells if the string view ends with another string view.
 bool StringView_endsWithView(string_view_t *, string_view_t *postfixToSearch);
-/**
- * Tells if the string view starts with another string view.
- */
+// Tells if the string view starts with another string view.
 bool StringView_startsWithView(string_view_t *, string_view_t *prefixToSearch);
-/**
- * Tells if the string view ends with a given string.
- */
+// Tells if the string view ends with a given string.
 bool StringView_endsWithString(string_view_t *, string_t *postfixToSearch);
-/**
- * Tells if the string view starts with a given string.
- */
+// Tells if the string view starts with a given string.
 bool StringView_startsWithString(string_view_t *, string_t *prefixToSearch);
-/**
- * Tells if the string view ends with a given C string.
- */
+// Tells if the string view ends with a given C string.
 bool StringView_endsWithCString(string_view_t *, const char *postfixToSearch);
-/**
- * Tells if the string view starts with a given C string.
- */
+// Tells if the string view starts with a given C string.
 bool StringView_startsWithCString(string_view_t *, const char *prefixToSearch);
 string_view_t StringView_find(string_view_t *, string_view_t *substring);
 bool StringView_contains(string_view_t *, string_view_t *substring);
@@ -124,20 +80,16 @@ string_view_t StringView_substring(string_view_t *, unsigned int start, unsigned
 void StringView_trimLeft(string_view_t *);
 void StringView_trimRight(string_view_t *);
 void StringView_trim(string_view_t *);
-/**
- * Compares the two given string views.
- * Returns 0 if they are equal.
- */
+// Compares the two given string views.
+// Returns 0 if they are equal.
 int StringView_compare(string_view_t *, string_view_t *);
 
-/**
- * These two macros help with printing these strings out with printf.
- * Example usage:
- * ```c
- * printf("Printing > "STRING_FORMAT"\n", STRING_ARGS(str));
- * ```
- * Can accept both string_t and string_view_t.
- */
+// These two macros help with printing these strings out with printf.
+// Example usage:
+// ```c
+// printf("Printing > "STRING_FORMAT"\n", STRING_ARGS(str));
+// ```
+// Can accept both string_t and string_view_t.
 #define STRING_FORMAT "%.*s"
 #define STRING_ARGS(STRING) STRING.length, STRING.data + STRING.start
 
