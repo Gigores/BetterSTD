b54e2a998011db6b974419124ed04494b887b98b
Priklen <igorsamodumskij@gmail.com>
2026-06-09 11:08:11 +0300

    update error.h

--- include/btrstd/error.h
+++ include/btrstd/error.h
@@ -1,5 +1,7 @@
 #pragma once
 
+#include "stdbool.h"
+
 typedef enum {
     BTR_OK,
     BTR_ERR,
@@ -21,6 +23,15 @@ void BTR_panicErrCode(int errCode, const char *format, ...);
     char *: BTR_panicNormal,                    \
     int: BTR_panicErrCode,                      \
 )(first __VA_OPT__(,) __VA_ARGS__)
+
+void BTR_panicNormalIf(bool condition, const char *format, ...);
+void BTR_panicErrCodeIf(bool condition, int errCode, const char *format, ...);
+
+#define BTR_panicIf(condition, second, ...) _Generic((second), \
+    const char *: BTR_panicNormalIf,                           \
+    char *: BTR_panicNormalIf,                                 \
+    int: BTR_panicErrCodeIf,                                   \
+)(condition, second __VA_OPT__(,) __VA_ARGS__)
 
 #define BTR_expect(result, message, ...) ({                 \
     __auto_type _r = (result);                              \
@@ -62,3 +73,30 @@ void BTR_panicErrCode(int errCode, const char *format, ...);
 
 #define BTR_Ok(T, Value) ((T) { .status = BTR_OK, .value = (Value) })
 #define BTR_Err(T, Error) ((T) { .status = BTR_ERR, .error = (Error) })
+
+#ifdef BTR_NO_PREFIX
+
+typedef btr_result_status_t result_status_t;
+#define Result BTR_Result
+
+#define panicNormal BTR_panicNormal
+#define panicErrCode BTR_panicErrCode
+#define panic BTR_panic
+
+#define panicNormalIf BTR_panicNormalIf
+#define panicErrCodeIf BTR_panicErrCodeIf
+#define panicIf BTR_panicIf
+
+#define expect BTR_expect
+#define unwrap BTR_unwrap
+#define unwrapOr BTR_unwrapOr
+#define unwrapErr BTR_unwrapErr
+#define try BTR_try
+
+#define isOk BTR_isOk
+#define isErr BTR_isErr
+
+#define Ok BTR_Ok
+#define Err BTR_Err
+
+#endif
