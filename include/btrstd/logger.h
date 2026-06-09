08bcd0d410894cfe4a8f0c8bc34470ff0cf45d10
Priklen <igorsamodumskij@gmail.com>
2026-06-09 17:04:35 +0300

    add debug* macros

--- include/btrstd/logger.h
+++ include/btrstd/logger.h
@@ -35,6 +35,31 @@ void BTR_logImpl(const char *file, int line, const char *func, btr_log_level_t l
 
 #define BTR_log(logLevel, formatString, ...) BTR_logImpl(__FILE__, __LINE__, __FUNCTION__, logLevel, formatString __VA_OPT__(,) __VA_ARGS__)
 
+// int
+#define BTR_debugInt(variable) BTR_log(LOG_DEBUG, #variable " = %d", variable)
+// unsigned int
+#define BTR_debugUInt(variable) BTR_log(LOG_DEBUG, #variable " = %u", variable)
+// long
+#define BTR_debugLong(variable) BTR_log(LOG_DEBUG, #variable " = %ld", variable)
+// unsigned long
+#define BTR_debugULong(variable) BTR_log(LOG_DEBUG, #variable " = %lu", variable)
+// long long
+#define BTR_debugLongLong(variable) BTR_log(LOG_DEBUG, #variable " = %lld", variable)
+// unsigned long long
+#define BTR_debugULongLong(variable) BTR_log(LOG_DEBUG, #variable " = %llu", variable)
+// char
+#define BTR_debugChar(variable) BTR_log(LOG_DEBUG, #variable " = %c", variable)
+// char * | const char *
+#define BTR_debugString(variable) BTR_log(LOG_DEBUG, #variable " = %s", variable)
+// void * | ...
+#define BTR_debugPointer(variable) BTR_log(LOG_DEBUG, #variable " = %p", variable)
+// float | double
+#define BTR_debugFloat(variable) BTR_log(LOG_DEBUG, #variable " = %f", variable)
+// size_t
+#define BTR_debugSize(variable) BTR_log(LOG_DEBUG, #variable " = %zu", variable)
+// bool
+#define BTR_debugBool(variable) BTR_log(LOG_DEBUG, #variable " = %s", variable ? "true" : "false")
+
 #ifdef BTR_NO_PREFIX
 
 #define LOG_LEVELS BTR_LOG_LEVELS
@@ -48,5 +73,18 @@ typedef btr_log_level_t log_level_t;
 
 typedef btr_logger_t logger_t;
 #define log BTR_log
+
+#define debugInt BTR_debugInt
+#define debugUInt BTR_debugUInt
+#define debugLong BTR_debugLong
+#define debugULong BTR_debugULong
+#define debugLongLong BTR_debugLongLong
+#define debugULongLong BTR_debugULongLong
+#define debugChar BTR_debugChar
+#define debugString BTR_debugString
+#define debugPointer BTR_debugPointer
+#define debugFloat BTR_debugFloat
+#define debugSize BTR_debugSize
+#define debugBool BTR_debugBool
 
 #endif  // BTR_NO_PREFIX
--- tests/logger.c
+++ tests/logger.c
@@ -11,9 +11,15 @@ int main(void)
     });
     BTR_log(LOG_DEBUG, "this is a test log");
     BTR_log(LOG_INFO, "this is a test log");
-    BTR_log(LOG_WARN, "this is a test log");
-    BTR_log(LOG_ERROR, "this is a test log");
+    BTR_log(LOG_WARN, "this is a %s log", "test");
+    BTR_log(LOG_ERROR, "this is a %d log", 12);
     BTR_log(LOG_FATAL, "this is a test log");
+    int var = 15;
+    BTR_debugInt(var);
+    double var2 = 15.312;
+    BTR_debugFloat(var2);
+    const char *var3 = "hell yeah";
+    BTR_debugString(var3);
     fclose(BTR_getLogger()->file);
     return 0;
 }
