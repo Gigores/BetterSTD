c7132c1874fb79d54bc0c593582bb652e008d179
Priklen <igorsamodumskij@gmail.com>
2026-06-13 11:36:02 +0300

    tweak allocator.h

--- include/btrstd/allocator.h
+++ include/btrstd/allocator.h
@@ -6,6 +6,9 @@
 
 typedef enum {
     BTR_ALLOC_ERR_OUT_OF_MEMORY,
+    BTR_ALLOC_ERR_UNSUPPORTED_OPERATION,
+    BTR_ALLOC_ERR_INVALID_POINTER,
+    BTR_ALLOC_ERR_INVALID_SIZE,
 } btr_alloc_err_t;
 
 typedef BTR_Result(void *, btr_alloc_err_t) btr_alloc_result_t;
@@ -34,11 +37,16 @@ void BTR_Allocator_deallocate(btr_allocator_t *, void *pointer);
 
 #ifdef BTR_NO_PREFIX
 
-typedef btr_alloc_err_t alloc_err_t;
+typedef btr_alloc_err_t    alloc_err_t;
 typedef btr_alloc_result_t alloc_result_t;
-typedef btr_allocator_t allocator_t;
+typedef btr_allocator_t    allocator_t;
 
-#define Allocator_allocate BTR_Allocator_allocate
+#define ALLOC_ERR_OUT_OF_MEMORY         BTR_ALLOC_ERR_OUT_OF_MEMORY
+#define ALLOC_ERR_UNSUPPORTED_OPERATION BTR_ALLOC_ERR_UNSUPPORTED_OPERATION
+#define ALLOC_ERR_INVALID_POINTER       BTR_ALLOC_ERR_INVALID_POINTER
+#define ALLOC_ERR_INVALID_SIZE          BTR_ALLOC_ERR_INVALID_SIZE
+
+#define Allocator_allocate   BTR_Allocator_allocate
 #define Allocator_reallocate BTR_Allocator_reallocate
 #define Allocator_deallocate BTR_Allocator_deallocate
 
