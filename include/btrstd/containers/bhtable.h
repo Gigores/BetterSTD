13119ff702a17a2f52d60aa62ea41754be4e84c0
Priklen <igorsamodumskij@gmail.com>
2026-06-16 22:18:12 +0300

    add docs

--- include/btrstd/containers/bhtable.h
+++ include/btrstd/containers/bhtable.h
@@ -4,11 +4,17 @@
 
 // Borrowing Hash Table
 
+// Borrowing hash table is a data structure that stores every item behind a specific key.
+// It is pretty fast, because the time coplexity of most essential operations is O(1),
+// unlike in just a list of key-value entries which will be O(n).
+
 typedef unsigned long long btr_hash_t;
 
+// Pre-made functions for creating borrowing hash tables with cstring keys.
 btr_hash_t BTR_hashCString(const void *);
 bool BTR_compareCString(const void *, const void *);
 
+// Pre-made functions for creating borrowing hash tables with int32 keys.
 btr_hash_t BTR_hashInt32(const void *);
 bool BTR_compareInt32(const void *, const void *);
 
@@ -27,16 +33,29 @@ typedef struct {
     btr_allocator_t *allocator;
 } btr_bhtable_t;
 
+// Creates a new empty borrowing hash table. It requires to function pointers to properly
+// work with needed keys.
+// The `allocator` parameter can be set as `NULL`, in this case it will use the global allocator.
 btr_bhtable_t BTR_BHTable_make(
     btr_hash_t (*hash)(const void *),
     bool (*compare)(const void *, const void *),
     btr_allocator_t *allocator
 );
+// Sets a key to a value in the borrowing hash table.
 void BTR_BHTable_put(btr_bhtable_t *, const void *key, const void *value);
+// Returns a value of the key in the borrowing hash table.
 btr_container_ptr_result_t BTR_BHTable_get(const btr_bhtable_t *, const void *key);
+// Returns a value of the key and removes it from the borrowing hash table.
 btr_container_ptr_result_t BTR_BHTable_pop(btr_bhtable_t *, const void *key);
+// Tells if the key has been set in the borrowing hash table.
 bool BTR_BHTable_contains(const btr_bhtable_t *, const void *key);
+// Returns the amount of keys in the borrowing hash table.
 size_t BTR_BHTable_len(const btr_bhtable_t *);
-void BTR_BHTable_clear(btr_bhtable_t *);
+// Returns a borrowing linked list containing all the keys from the
+// borrowing hash table.
+// You will need to free this list yourself.
 btr_bllist_t BTR_BHTable_keys(const btr_bhtable_t *);
+// Deallocates the entire borrowing hash table, making it unusable afterwards.
 void BTR_BHTable_free(btr_bhtable_t *);
+// Deallocates the entire borrowing hash table, but reinitializes it to be empty.
+void BTR_BHTable_clear(btr_bhtable_t *);
