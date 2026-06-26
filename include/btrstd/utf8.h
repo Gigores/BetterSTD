32bc5f3f4daf4e03574497a559b49a35c3a3b9bd
Priklen <igorsamodumskij@gmail.com>
2026-06-26 21:21:51 +0300

    add NO_PREFIX to utf8

--- include/btrstd/utf8.h
+++ include/btrstd/utf8.h
@@ -9,3 +9,11 @@ size_t BTR_UTF8_charLen(unsigned char);
 // Tells if the given byte is not first
 // of its UTF-8 character.
 bool BTR_UTF8_isContinuation(unsigned char);
+
+#ifdef BTR_NO_PREFIX
+
+#define UTF8_charLen        BTR_UTF8_charLen
+#define UTF8_isContinuation BTR_UTF8_isContinuation
+
+#endif  // BTR_NO_PREFIX
+
